e6f6674a227d8be21174849017b97fb2e132f81f
Abhishek Kushwah <abhishek_kushwah@epam.com>
2023-06-05 16:35:50 +0530

    change db.json

--- api/server.js
+++ api/server.js
@@ -1,13 +1,18 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const server = jsonServer.create()
+const server = jsonServer.create();
+
 const router = jsonServer.router('db.json')
+// const fs = require('fs')
+// const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+// const router = jsonServer.router(db)
+
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
+    '/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
